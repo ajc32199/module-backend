cc9603bf527edd349b8128ac56e7bb9aa6782bd1
chiar015 <chiar015@d.umn.edu>
2024-08-21 10:35:53 -0500

    added front end connection

--- server.js
+++ server.js
@@ -4,28 +4,11 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const uri =
-  "mongodb+srv://adamchiarella:YStb4pt9flcJz5Re@membermodule.qbpep.mongodb.net/?retryWrites=true&w=majority&appName=MemberModule";
 
-//middleware
+app.get("/api", (req, res) => {
+  res.json({ users: ["user1", "user2", "user3"] });
+});
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/members", require("./routes/Members"));
-
-//MongoDB connection
-mongoose
-  .connect(uri, {})
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.log("MongoDB connection failed", err);
-  });
-
-//start the server
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
